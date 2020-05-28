89a1407d5ce13f703fbb4443299121997e48126d
Dominik.Kotecki <dominikus1910@gmail.com>
2020-05-28 20:36:21 +0200

    google

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import Discord from "discord.js";
-import {google} from 'googleapis';
+import { google } from 'googleapis';
 import { getCommands, getCommand } from "./commands/comamnd";
 import { Option, map, isSome } from 'fp-ts/lib/Option'
 
-const yt = google.youtube({ version: "v3", auth: process.env.GOOGLE_API_KEY})
+const yt = google.youtube({ version: "v3", auth: process.env.GOOGLE_API_KEY })
 
 const client = new Discord.Client();
 
 client.on("ready", () => {
-    console.log("Elo")
+  console.log("Elo")
 })
 const commands = getCommands({ yt: yt });
 const command = getCommand(commands);
 client.on('message', msg => {
-    const cmd = command(msg);
-    if (isSome(cmd)) {
-      const c = cmd.value;
-      c.execute(msg);
-    }
+  const cmd = command(msg);
+  if (isSome(cmd)) {
+    const c = cmd.value;
+    c.execute(msg);
+  }
 });
 console.log(process.env.DISCORD_TOKEN)
 client.login(process.env.DISCORD_TOKEN)
