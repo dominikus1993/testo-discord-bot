712baa2936ad1c635d813adde113d00660f2be4f
Dominik.Kotecki <dominikus1910@gmail.com>
2020-05-28 21:05:10 +0200

    add order type by relevance

--- src/commands/searchYoutube.ts
+++ src/commands/searchYoutube.ts
@@ -7,7 +7,8 @@ export function getSearchYoutubeCommand(yt: youtube_v3.Youtube): () => ICommand 
         name: "wyszukaj",
         execute: (msg, args) => {
             const videoQ = msg.content.replace("!wyszukaj", "")
-            yt.search.list({ part: "snippet", type: "video", q: `Testoviron ${videoQ}`, maxResults: 1, order: "date"}).then(res => {
+            yt.search.list({ part: "snippet", type: "video", q: `Testoviron ${videoQ}`, maxResults: 1, order: "relevance" }).then(res => {
+                console.log(res)
                 for (const item of res.data.items?.filter(x => x.id?.kind == "youtube#video") ?? []) {
                     if(item.id){
                         msg.reply(`Poczęstuj się https://www.youtube.com/watch?v=${item.id?.videoId}`)
