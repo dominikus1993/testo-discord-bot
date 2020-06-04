5f7f1793a568385f0bbae704dc9f3203dba57359
Dominik.Kotecki <dominikus1910@gmail.com>
2020-06-04 14:12:08 +0200

    use rx from event

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Discord from "discord.js";
 import { google } from 'googleapis';
-import { getCommands, getCommand } from "./commands/comamnd";
-import { Option, map, isSome } from 'fp-ts/lib/Option'
+import { getCommands, getCommand, ICommand } from "./commands/comamnd";
+import { Option, Some, map, isSome } from 'fp-ts/lib/Option'
+import { fromEvent } from "rxjs";
+import { filter, map as omap, flatMap } from 'rxjs/operators';
 
 const yt = google.youtube({ version: "v3", auth: process.env.GOOGLE_API_KEY })
 
@@ -12,13 +14,15 @@ client.on("ready", () => {
 })
 const commands = getCommands({ yt: yt });
 const command = getCommand(commands);
-client.on('message', msg => {
-  const cmd = command(msg);
-  if (isSome(cmd)) {
-    const c = cmd.value;
-    c.execute(msg);
-  }
-});
+
+fromEvent(client, "message")
+  .pipe(omap((m: Discord.Message) => ({ msg: m, cmd: command(m) })))
+  .pipe(filter(({ cmd }) => isSome(cmd)))
+  .subscribe(({cmd, msg}) => {
+    const c = (cmd as Some<ICommand>).value;
+    c.execute(msg)
+  });
+
 console.log(process.env.DISCORD_TOKEN)
 client.login(process.env.DISCORD_TOKEN)
 
